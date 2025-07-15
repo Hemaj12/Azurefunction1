c5b3e275a07c9f35cf20fe2119b4a46517081e32
Hemaj12 <hema@webanix.in>
2025-07-15 17:46:16 +0530

    Update Function1.cs
    Corrected the syntax of cron expression.

--- IDEXCustAzure/Function1.cs
+++ IDEXCustAzure/Function1.cs
@@ -26,7 +26,7 @@ namespace IDEXCustAzure
 
         
         [Function("Function1")]    
-        public async Task Run([TimerTrigger("*0 */2 * * * *")] TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
         {
             try
             {
